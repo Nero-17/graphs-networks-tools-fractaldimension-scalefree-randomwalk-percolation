5dd10ffec9de57e5e17e382d2dfa3fc1b864afa0
Nero-17 <neroziyuli@gmail.com>
2025-12-06 23:45:54 +0000

    Update __init__.py

--- graphs_networks_tools/__init__.py
+++ graphs_networks_tools/__init__.py
@@ -1,13 +1,13 @@
 # graphs_networks_tools/__init__.py
 
-from .graphs_networks_tools import cpu_compute_box_dimension
+from .graphs_networks_tools import cpu_compute_fractal_dimension
 
 try:
-    from .graphs_networks_tools import gpu_compute_box_dimension
+    from .graphs_networks_tools import gpu_compute_fractal_dimension
 except Exception:
     gpu_compute_box_dimension = None
 
-from .graphs_networks_tools import compute_degree_dimension
+from .graphs_networks_tools import compute_scale_free_exponent
 
 __all__ = [
     "cpu_compute_fractal_dimension",
