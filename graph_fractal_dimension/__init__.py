9e070a2ad66a3e01b844e98f44ce083b51af4e72
Nero-17 <neroziyuli@gmail.com>
2025-12-06 23:11:34 +0000

    Delete graph_fractal_dimension/__init__.py

--- graph_fractal_dimension/__init__.py
+++
@@ -1,9 +0,0 @@
-from .box_dimension import cpu_compute_box_dimension
-
-try:
-    from .box_dimension_gpu import gpu_compute_box_dimension
-except Exception:
-    # GPU dependencies (torch, CUDA) might be missing.
-    gpu_compute_box_dimension = None
-
-__all__ = ["cpu_compute_box_dimension", "gpu_compute_box_dimension"]
