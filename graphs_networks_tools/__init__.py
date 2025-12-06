4281072ebd011b6fcabbda9adbbbd30e0bc05731
Nero-17 <neroziyuli@gmail.com>
2025-12-06 23:31:39 +0000

    Update __init__.py

--- graphs_networks_tools/__init__.py
+++ graphs_networks_tools/__init__.py
@@ -1,14 +1,7 @@
 # graphs_networks_tools/__init__.py
 
-"""
-Graphs Networks Tools:
-- Box-counting (fractal) dimension of graphs (CPU + GPU)
-- Degree dimension (degree exponent / scale-free exponent)
-"""
-
 from .box_dimension import cpu_compute_box_dimension
 
-# GPU version is optional: import if available, otherwise set to None
 try:
     from .box_dimension_gpu import gpu_compute_box_dimension
 except Exception:
