94994cea5573792f928d2922d2cce674bbc7bac7
Nero-17 <neroziyuli@gmail.com>
2025-12-06 17:59:02 +0000

    Update __init__.py

--- graph_fractal_dimension/__init__.py
+++ graph_fractal_dimension/__init__.py
@@ -1,5 +1,9 @@
-# graph_fractal_dimension/__init__.py
-
 from .box_dimension import cpu_compute_box_dimension
 
-__all__ = ["cpu_compute_box_dimension"]
+try:
+    from .box_dimension_gpu import gpu_compute_box_dimension
+except Exception:
+    # GPU dependencies (torch, CUDA) might be missing.
+    gpu_compute_box_dimension = None
+
+__all__ = ["cpu_compute_box_dimension", "gpu_compute_box_dimension"]
